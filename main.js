7d72f2745001d5bd7260bfdf5f2dd67feb687688
Sagar Iqbal <sagari17@uia.no>
2019-10-19 17:28:47 +0200

    Added Procfile for heroku

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const PORT = process.env.PORT || 5000
 
 http.createServer(function (req, res)
 {
@@ -25,6 +26,6 @@ http.createServer(function (req, res)
         res.write(data);
         return res.end();
     })
-}).listen(8080);
+}).listen(PORT);
 
 console.log("Server listening on port 8080...");

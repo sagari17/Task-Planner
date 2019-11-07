c004e13192724a60f2f5d4a08e158e7c5d3109a5
s1710238044 <32874952+s1710238044@users.noreply.github.com>
2019-11-07 12:24:35 +0100

    menuBar and some styles for it

--- public/js/index.js
+++ public/js/index.js
@@ -14,3 +14,21 @@ function redirectNotAuthUser(websitePath) {
     window.location.href = websitePath;
   }
 }
+
+var utilities = utilities || {};
+utilities = (function() {
+  async function requestToServer(path, content) {
+    return fetch(path, content).then(resp => {
+      if (resp.ok) {
+        return resp.json();
+      } else if (resp.status > 202) {
+        throw resp.json();
+      }
+    });
+  }
+  return {
+    requestToServer: requestToServer
+  };
+})();
+
+console.log("utilities works");

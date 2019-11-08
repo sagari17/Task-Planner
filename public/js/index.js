6f91b5e6f4b3dba983d5036c837713c6058e4627
s1710238044 <32874952+s1710238044@users.noreply.github.com>
2019-11-08 14:23:18 +0100

    fix 1

--- public/js/index.js
+++ public/js/index.js
@@ -19,6 +19,7 @@ var utilities = utilities || {};
 utilities = (function() {
   async function requestToServer(path, content) {
     return fetch(path, content).then(resp => {
+      console.log(resp);
       if (resp.ok) {
         return resp.json();
       } else if (resp.status > 202) {

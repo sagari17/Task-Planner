3a9995400a09448ddc156a9d6e0fb0fe08a9f7ed
s1710238044 <32874952+s1710238044@users.noreply.github.com>
2019-11-24 20:57:44 +0100

    correct endpoint protection

--- routes/lists.js
+++ routes/lists.js
@@ -120,7 +120,7 @@ router.get("/view/:listID", async function(req, res, next) {
   }
 });
 
-router.delete("/view/:listID", protectEndpoints);
+router.delete("/:listID", protectEndpoints);
 // delete list -------------------------------------------------------------------
 router.delete("/:listID", async function(req, res, next) {
   try {
